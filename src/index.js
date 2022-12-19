2f7bbead3a1cd52e8e8b42e68de0b6bb36b2bd46
ABiteOfSorrow <laceican@naver.com>
2022-12-19 16:10:26 +0100

    project setting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <>
+    <h1 id="title">Pierre-Papier-Ciseaux</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <button className="hand">Pierre</button>
+    <button className="hand">Papier</button>
+    <button className="hand">Ciseaux</button>
+    </>
+);
