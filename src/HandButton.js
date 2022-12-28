ba3aef60a44c9a230cd6ad572162e4c903e83395
ABiteOfSorrow <laceican@naver.com>
2022-12-28 18:51:10 +0100

    Add inline-styles for handButtons

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,10 +1,27 @@
 import HandIcon from "./HandIcon";
+import buttonBack from "./assets/purple.svg";
+
+
+const HandButtonStyle = {
+    width: '166px',
+    height: '166px',
+    border: 'none',
+    outline: 'none',
+    textAlign: 'center',
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    backgroundImage: `url('${buttonBack}')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
+  }
+
 
 function HandButton({value, onClick}) {
     let handleClick = () => onClick(value);
 
     return (
-        <button onClick={handleClick}>
+        <button style={HandButtonStyle} onClick={handleClick}>
             <HandIcon value={value} />
         </button>
     );
